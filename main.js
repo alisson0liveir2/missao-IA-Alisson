34cb5d5a79678c2feb56657020bda13da4fe0a41
alisson0liveir2 <145785362+alisson0liveir2@users.noreply.github.com>
2025-10-07 11:42:24 -0300

    profe

---
+++ main.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+const perguntas = [
+    {
+        enunciado: " Quando você está aprendendo algo novo, o que mais te ajuda?",
+        alternativas: [
+            {
+                texto: " Ver imagens, esquemas, gráficos ou mapas.",
+                afirmacao: " Você aprende melhor quando consegue visualizar as informações em diagramas e imagens.",
+            },
+            {
+                texto: " Ouvir explicações e conversar sobre o assunto.",
+                afirmacao: " Você valoriza ouvir boas explicações e trocar ideias para consolidar o conhecimento. ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: "Afirmação",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+];
